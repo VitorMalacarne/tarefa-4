4346bfadd525b14dd14e1345d4070002f472ba88
VitorMalacarne <vitorhenriquewm@gmail.com>
2022-12-02 08:38:31 -0300

    tarifas

--- admin/sub/view_adm/form_tarifa.php
+++ admin/sub/view_adm/form_tarifa.php
@@ -10,7 +10,8 @@
 </head>
 
 <body>
-    <nav id="mainnav">
+    <header>
+    <nav id="headnav">
         <ul>
     	    <li><a href="index.html">HOME</a></li>
 	        <li><a href="list_acomodacao.php">Lista de acomodações</a></li>
@@ -21,50 +22,13 @@
     		<li><a href="form_tarifa.php">Formulário de tarifas</a></li>
     	 </ul>
     </nav>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">ADM MODE</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="lista_users.php">Users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="list_acom.php">Acomodações</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="list_tarifas.php">Tarifas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="list_reservas.php">Reservas</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+    </header>
+    
     <div class="container">
-        <h3>Nova tarifa</h3>
 
-
-        <!-- <?php if (@$message) : ?>
-
-            <div class="toast fade show align-items-center text-bg-warning border-0 mx-auto my-3" role="alert" aria-live="polite" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body">
-                        <?= @$message ?>
-                    </div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>
-
-        <?php endif; ?> -->
-
-        <form action="../controller/controller.tar.php" method="POST">
+        <form action="ctrl_tarifa.php" method="POST">
+            <fieldset>
+            <legend>Nova tarifa</legend>
             <input type="hidden" name="action" value="cadastrar">
             <input type="hidden" name="id" value="<?= @$tarifa->id ?>">
 
@@ -83,11 +47,12 @@
                 <input required type="number" name="precoA" value="<?= @$tarifa->precoA ?>" class="form-control">
             </div>
 
-
+            </fieldset>
             <div>
-                <button class="btn btn-success mt-3" type="submit">Salvar</button>
-                <a href="../view_adm/list_tarifas.php" class="btn btn-light mt-3 ms-1">Cancelar</a>
+                <input  type="submit" value="Salvar">
+                <a href="list_tarifa.php" >Cancelar</a>
             </div>
+            
 
         </form>
     </div>
