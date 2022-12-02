95fde4e68f293088c3e0a32736cae4a0a1911bd0
Fellyipe <felipebueno2004@hotmail.com>
2022-12-02 20:57:53 -0300

    Alterações

--- admin/sub/view_adm/ctrl_usuario.php
+++ admin/sub/view_adm/ctrl_usuario.php
@@ -17,18 +17,25 @@ if(@$action == 'novo') {
     echo "TESTE";
     
     if(@$_POST['senha'] == @$_POST['confirmar_senha']){
-        $usuarioDAO->insert(@$_POST);
-        $view = '../view_usuario/principal.php';
-
-        $id = $usuarioDAO->getUsuarioByEmail(@$_POST['email'])->id;
-        echo $id;
-        $_SESSION['id'] = $id;
-        $_SESSION['nome'] = @$_POST['nome'];
-
-        header('location: '.$view);
+        
+        if(empty($usuarioDAO->getUsuarioByEmail(@$_POST['email']))){
+            
+            $usuarioDAO->insert(@$_POST);
+            $view = '../view_usuario/principal.php';
+            
+            $id = $usuarioDAO->getUsuarioByEmail(@$_POST['email'])->id;
+            echo $id;
+            $_SESSION['id'] = $id;
+            $_SESSION['nome'] = @$_POST['nome'];
+            
+            header('location: '.$view);
+            
+        }
+        $message = "O email ".$_POST['email']." já está sendo utilizado, escolha outro";
+        require_once('../view_usuario/cadastro.php');
     }else{
         $message = "As duas senhas devem ser iguais";
-        $view = "../view_user/cadastro.php";
+        $view = "../view_usuario/cadastro.php";
     require_once($view);
     }
 
@@ -48,17 +55,17 @@ if(@$action == 'novo') {
 
     $usuario = $usuarioDAO->getUsuarioByEmail($email);
 
-    if(empty($user)){
+    if(empty($usuario)){
         $message = "Digite um email válido";
         $view = "../view_usuario/login.php";
         require_once($view);
     }else{
-        if(@$_POST['pass'] == $usuario->senha){
+        if(@$_POST['senha'] == $usuario->senha){
             //iniciar a sessão
             $_SESSION['id'] = $usuario->id;
             $_SESSION['nome'] = $usuario->nome;
             //$view = @$url;
-            header('location: ../view_usuario/reserva.html');
+            header('location: ../view_usuario/principal.php');
         }else{
             $message = "Senha incorreta";
             $view = "../view_usuario/login.php";
