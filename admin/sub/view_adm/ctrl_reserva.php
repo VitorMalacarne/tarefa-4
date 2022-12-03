675905552a337c3f42378e965e0faefeeeff44bf
Fellyipe <felipebueno2004@hotmail.com>
2022-12-02 21:56:08 -0300

    Alterações

--- admin/sub/view_adm/ctrl_reserva.php
+++ admin/sub/view_adm/ctrl_reserva.php
@@ -14,29 +14,40 @@ $action = @$_REQUEST['action'];
 
 // Decidir qual ação será tomada
 if($action == 'reservar') {
-    if(empty($_SESSION)){
-        $view = "../view_usuario/login.php";
-        require_once($view);
-    }else{
-        $id_tarifa = $_POST['id_tarifa'];
-        require_once('ctrl_tarifa.php');
-        $tarifa = $tarifaDAO->getById($id_tarifa);
-        echo "Depois de tarifadao";
-        $id_usuario = $_SESSION['id'];
-        $qtd_pessoas = intval($_REQUEST['qtd_adultos']) + intval($_REQUEST['qtd_criancas']);
-        $valor_reserva = ($tarifa->preco + $tarifa->preco_adulto * (intval($_REQUEST['qtd_adultos']) - 1) + $tarifa->preco_crianca * intval($_REQUEST['qtd_criancas']));
-
-        $data_entrada = @$_REQUEST['data_entrada'];
-        $data_saida = @$_REQUEST['data_saida'];
-        echo $data_entrada;
-        echo $data_saida;
-
-        $reservaDAO->insert(@$_POST, $id_usuario, $qtd_pessoas, $valor_reserva, $data_entrada, $data_saida);
-
-        $view = "../view_usuario/principal.php";
+    
+    $data_entrada = @$_REQUEST['data_entrada'];
+    $data_saida = @$_REQUEST['data_saida'];
+    if($data_entrada >= $data_saida){
+        $message = "A data de entrada deve ser anterior à data de saída";
+        $view = "../view_usuario/reserva.php";
         header('location: ' . $view);
     }
-
+    $id_tarifa = @$_REQUEST['id_tarifa'];
+    require_once('ctrl_tarifa.php');
+    $tarifa = $tarifaDAO->getById($id_tarifa);
+    echo "Depois de tarifadao";
+    $reservas_mesmo_id = $reservaDAO->getAllDatas(@$_REQUEST['id_acomodacao']);
+    foreach($reservas_mesmo_id as $index => $reserva_mesmo_id){
+        if($reserva_mesmo_id['data_entrada'] >= $data_entrada && $reserva_mesmo_id['data_entrada'] < $data_saida){
+            $message = "Conflito de datas!";
+            $view = "../view_usuario/reserva.php";
+            header('location: ' . $view);
+        }
+        if($reserva_mesmo_id['data_saida'] >= $data_entrada && $reserva_mesmo_id['data_saida'] < $data_saida){
+            $message = "Conflito de datas!";
+            $view = "../view_usuario/reserva.php";
+            header('location: ' . $view);
+        }
+    }
+    $id_usuario = $_SESSION['id'];
+    $qtd_pessoas = intval($_REQUEST['qtd_adultos']) + intval($_REQUEST['qtd_criancas']);
+    $valor_reserva = ($tarifa->preco + $tarifa->preco_adulto * (intval($_REQUEST['qtd_adultos']) - 1) + $tarifa->preco_crianca * intval($_REQUEST['qtd_criancas']));
+    echo $data_entrada;
+    echo $data_saida;
+    $reservaDAO->insert(@$_POST, $id_usuario, $qtd_pessoas, $valor_reserva, $data_entrada, $data_saida);
+    $view = "../view_usuario/principal.php";
+    header('location: ' . $view);
+    
 } else if($action == 'deletar') {
     $id = @$_REQUEST['id'];
 
